611b73690b65ff64e782d1fe5e229d401db78f6a
kiduyu-klaus <muigakiduyu1995@gmail.com>
2020-09-08 15:02:39 +0300

    Fixed Profile Fragment

--- app/src/main/java/com/kiduyu/wambuiproject/k_adoptiom/ProfileFragment.java
+++ app/src/main/java/com/kiduyu/wambuiproject/k_adoptiom/ProfileFragment.java
@@ -1,8 +1,11 @@
 package com.kiduyu.wambuiproject.k_adoptiom;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
@@ -10,11 +13,14 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -25,10 +31,11 @@ public class ProfileFragment extends Fragment {
     EditText edtFullName;
     EditText edtEmail;
     EditText edtPassword;
-    Button edt_profile;
+    Button edt_profile, save;
     EditText edtMobile;
     Toolbar toolbar;
     ProgressDialog pDialog;
+    String maddress="";
     Menu menu;
 
     @Override
@@ -40,6 +47,7 @@ public class ProfileFragment extends Fragment {
 
         edtFullName = view.findViewById(R.id.edt_name);
         edtFullName.setEnabled(false);
+        save = view.findViewById(R.id.edt_profile_save);
         edt_profile = view.findViewById(R.id.edt_profile);
         edtEmail = view.findViewById(R.id.edt_email);
         edtEmail.setEnabled(false);
@@ -78,10 +86,103 @@ public class ProfileFragment extends Fragment {
                 edtMobile.setHint("Input New mobile number");
                 edtMobile.setHintTextColor(getResources().getColor(R.color.dialog_color));
 
+
+                edt_profile.setVisibility(View.GONE);
+                save.setVisibility(View.VISIBLE);
+
             }
         });
 
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = edtFullName.getText().toString();
+                String phone = edtMobile.getText().toString();
+                String memail = edtMobile.getText().toString();
+                String password = edtPassword.getText().toString();
+
+
+
+                if (TextUtils.isEmpty(name))
+                {
+                    edtFullName.setError("Name Is Required..");
+                    return;
+                }
+                else if (TextUtils.isEmpty(phone))
+                {
+                    edtMobile.setError("Phone Number Is Required..");
+                    return;
+                }else if (TextUtils.isEmpty(memail))
+                {
+                    edtMobile.setError("Phone Number Is Required..");
+                    return;
+                }
+                else if (TextUtils.isEmpty(password))
+                {
+                    edtPassword.setError("Password Is Required..");
+                    return;
+                }
+                else
+                {
+                    String first3 ="";
+                    first3=phone.substring(0 , 3);
+                    if (!first3.equals("254")){
+                        edtMobile.setError("Phone Number must start with 254..");
+                        return;
+                    }else {
+                        pDialog.setTitle("Updating Account");
+                        pDialog.setMessage("Please wait, while we are checking the credentials.");
+                        pDialog.setCanceledOnTouchOutside(false);
+                        pDialog.show();
+                        //String uniqueid= UUID.randomUUID().toString();
+                        String uniqueid= Settings.Secure.getString(getActivity().getContentResolver(), Settings.Secure.ANDROID_ID);
+                        Log.d("TAG", "onCreate: "+uniqueid);
+
+                        ValidatephoneNumber(uniqueid,name, phone,memail,maddress, password);
+                    }
+
+                }
+            }
+        });
         return view;
+    }
+
+    private void ValidatephoneNumber(final String uniqueid, final String name, final String phone, final String memail, final String maddress, final String password) {
+        final DatabaseReference RootRef;
+        RootRef = FirebaseDatabase.getInstance().getReference();
+        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                User userdata = new User(uniqueid,name,phone,memail,maddress,password);
+                RootRef.child("Users").child(uniqueid).setValue(userdata).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful())
+                        {
+                            Toast.makeText(getActivity(), "Congratulations "+name+", your account has been Updated.", Toast.LENGTH_SHORT).show();
+                            pDialog.dismiss();
+
+                            getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_frame,new ProfileFragment()).commit();
+                        }
+                        else
+                        {
+                            pDialog.dismiss();
+                            Toast.makeText(getActivity(), "Network Error: Please try again after some time...", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getActivity(), String.valueOf(databaseError.getMessage()), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     private void GetProfile(final String uniqueid) {
@@ -97,6 +198,7 @@ public class ProfileFragment extends Fragment {
                     edtFullName.setText(usersData.getName());
                     edtFullName.setTextSize(23.00f);
 
+                    maddress=usersData.getAddress();
                     edtEmail.setText(usersData.getEmail());
                     edtEmail.setTextSize(23.00f);
 
