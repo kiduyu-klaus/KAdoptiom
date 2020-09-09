c642771bc8a76c6d90cb9e8fd4d585927248dedf
kiduyu-klaus <muigakiduyu1995@gmail.com>
2020-09-09 12:03:54 +0300

    Created kids details

---
+++ app/src/main/java/com/kiduyu/wambuiproject/k_adoptiom/KidDetails.java
@@ -0,0 +1,14 @@
+package com.kiduyu.wambuiproject.k_adoptiom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KidDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kid_details);
+    }
+}
